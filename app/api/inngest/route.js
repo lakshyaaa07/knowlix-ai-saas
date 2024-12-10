06e31a735e723023281da048c5aef332e9cc848f
Lakshya Jain <112760813+lakshyaaa07@users.noreply.github.com>
2024-12-10 11:27:41 +0530

    yep

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createNewUser, generateNotes, generateStudyTypeContent, helloWorld } from "@/inngest/functions";
-export const runtime = 'edge';
+// export const runtime = 'edge';
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
